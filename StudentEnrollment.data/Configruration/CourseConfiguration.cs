846410c16bca49d9daba8592d0090b0635738462
Ray Brown-Amory <r.brown-amory@sky.com>
2023-01-23 09:16:29 +0000

    Compeleted the entity database

---
+++ StudentEnrollment.data/Configruration/CourseConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StudentEnrollment.data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEnrollment.data.Configruration
+{
+    internal class CourseConfiguration : IEntityTypeConfiguration<Course>
+    {
+        public void Configure(EntityTypeBuilder<Course> builder)
+        {
+            builder.HasData(
+                 new Course
+                 {
+                     Id = 1,
+                     Title = "Minimal API development",
+                     Credits = 3
+                    
+                 },
+                 new Course
+                 {
+                     Id = 2,
+                     Title = "Ultimate API development",
+                     Credits = 4
+                 }
+                );
+        }
+    }
+}
--- StudentEnrollment.data/StudentEnorllmentDbContext.cs
+++ StudentEnrollment.data/StudentEnorllmentDbContext.cs
@@ -1,11 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using StudentEnrollment.data.Configruration;
 using StudentEnrollment.data.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StudentEnrollment.data
 {
@@ -14,6 +11,13 @@ namespace StudentEnrollment.data
         public StudentEnorllmentDbContext(DbContextOptions<StudentEnorllmentDbContext> options) :base(options)
         {
                 
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new CourseConfiguration());
+            builder.ApplyConfiguration(new UserRoleCourseConfiguration());
         }
         public DbSet<Course> Courses { get; set; } 
         public DbSet<Student> Students { get; set; } 
